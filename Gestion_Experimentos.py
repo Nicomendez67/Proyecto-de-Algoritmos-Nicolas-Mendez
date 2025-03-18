7a8f99d33235f987e3211e6dd1b089ae61559a37
Nicomendez67 <n.mendez@correo.unimet.edu.ve>
2025-03-18 13:56:47 -0400

    Gestion_Experimentos.py

--- Gestion_Experimentos.py
+++ Gestion_Experimentos.py
@@ -3,7 +3,7 @@ from Experimentos import Experimento
 
 class Gestion_experimentos():
     
-    def crear_experimento(app, experimentos_json):
+    def crear_experimento(labo, experimentos_json):
         experimento_objetos=[]
         for experimento in experimentos_json:
             experimento_objetos.append(Experimento(experimento["id"],experimento["receta_id"],experimento["personas_responsables"],experimento["fecha"],experimento["costo_asociado"],experimento["resultado"]))
