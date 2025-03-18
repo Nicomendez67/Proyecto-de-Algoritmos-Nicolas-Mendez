2612fff635d8c95ee436895019296ce186636110
Nicomendez67 <n.mendez@correo.unimet.edu.ve>
2025-03-18 13:55:48 -0400

    Gestion_recetas.py

--- Gestion_recetas.py
+++ Gestion_recetas.py
@@ -2,7 +2,7 @@ from recetas import Recetas
 
 class Gestion_recetas():
     
-    def crear_recetas(app,recetas_json):
+    def crear_recetas(labo,recetas_json):
         recetas_objetos=[]
         for receta in recetas_json:
             recetas_objetos.append(Recetas(receta["id"], receta["nombre"], receta["objetivo"], receta["reactivos_utilizados"], receta["procedimiento"], receta["valores_a_medir"]))
